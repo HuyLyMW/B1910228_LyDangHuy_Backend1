adf731abfc475bf3acfc9c11b9fb0c838bb97bbb
HuyLyMW <huylymw@gmail.com>
2023-02-24 12:01:30 +0700

    Cai dat cac handler truy xuat CSDL

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.get("/", (req, res) => {
 
 app.use("/api/contacts", contactsRouter);
 // handle 404 response
-app.use((req, res, next) => {
+app.use((_req, res, next) => {
     // Code ở đây sẽ chạy khi không có route được định nghĩa nào
     // khớp với yêu cầu. Gọi next() để chuyển sang middleware xử lý lỗi
     return next(new ApiError(404, "Resource not found"));
